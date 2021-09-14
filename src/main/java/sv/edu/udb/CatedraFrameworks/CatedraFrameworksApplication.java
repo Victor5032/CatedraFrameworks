7139c8f9501ad6903dacf346a321faf0e31d5fcc
Victor5032 <victorlopez_20@outlook.com>
2021-09-13 21:38:46 -0600

    Cositas grandes se vienen UwU

---
+++ src/main/java/sv/edu/udb/CatedraFrameworks/CatedraFrameworksApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.CatedraFrameworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatedraFrameworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatedraFrameworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/edu/udb/CatedraFrameworks/CatedraFrameworksApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.CatedraFrameworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatedraFrameworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
