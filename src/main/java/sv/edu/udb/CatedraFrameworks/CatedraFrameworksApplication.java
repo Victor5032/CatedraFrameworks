9a28c3fb78b831dc8c971fcf1403e50d3f731fa0
Victor5032 <victorlopez_20@outlook.com>
2021-09-25 15:07:34 -0600

    Dependencia H2Database y configuración en archivo properties

--- src/main/java/sv/edu/udb/CatedraFrameworks/CatedraFrameworksApplication.java
+++ src/main/java/sv/edu/udb/CatedraFrameworks/CatedraFrameworksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatedraFrameworksApplication {
-    //el inicio de algo grande uwu
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CatedraFrameworksApplication.class, args);
 	}
